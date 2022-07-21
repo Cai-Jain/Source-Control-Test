4a9b73c13d29737ed575796bb491b6cfa66eea79
Cai Jain <caijain24@gmail.com>
2022-07-21 14:21:28 -0500

    Initial Commit

---
+++ Source Control Test/Source_Control_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_Control_TestApp.swift
+//  Source Control Test
+//
+//  Created by Cai Jain on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Source_Control_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
